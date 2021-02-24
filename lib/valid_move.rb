76db21d067eaac5e62f9f5d893dc90c16a09f7ac
Chelsea Anne Ashworth <ashworthca@gmail.com>
2021-02-24 15:37:54 -0500

    commit tree 66a9d8b3080560fea8f7a2c75cfea91397e0bdbd

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,20 @@
 # code your #valid_move? method here
-
+def valid_move?(board,index)
+    if position_taken?(board, index) == true
+        valid = false
+    elsif (board[index] == "" || board[index] == " ")
+        valid = true
+    else
+        valid = nil 
+    end      
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+    taken = nil
+    if (board[index] == "" || board[index] == " " || board[index] == nil)
+        taken = false
+    else
+        taken = true
+    end
+end
